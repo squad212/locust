1a2d2f4e51f2ccf78849986885aaba82abdeaf58
kcsar <kcsar@DESKTOP-GRMRODS.lan>
2021-03-24 14:31:25 -0500

    Locust Program is Launch.

---
+++ locust/src/main/java/com/example/demo/LocustApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocustApplication.class, args);
+	}
+
+}
