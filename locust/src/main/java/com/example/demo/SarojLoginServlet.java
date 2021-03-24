1deeaf0821c3af267959b7d6aa77aec090e007f0
kcsar <kcsar@DESKTOP-GRMRODS.lan>
2021-03-24 17:02:37 -0500

    new change in application properties. more library importing code added in locust application and sarojloginservlet. few changes made in index too.

--- locust/src/main/java/com/example/demo/SarojLoginServlet.java
+++ locust/src/main/java/com/example/demo/SarojLoginServlet.java
@@ -1,8 +1,62 @@
 package com.example.demo;
 
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
-public class SarojLoginServlet {
+public class SarojLoginServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		//1.2.3.4 
+		//to find the customer ip address
+		String uid = req.getParameter("username");
+		String pwd = req.getParameter("password");
+		System.out.println( req.getQueryString() );
+		try {
+			if(uid.equals("saroj") && pwd.equals("123")) {
+				res.getWriter().write(" login is successful ");
+			
+			
+		
+			 // text format response
+				
+			req.getSession().setAttribute("login", "success");
+			res.sendRedirect("welcome.html");
+			}
+			
+			else {
+				req.getSession().setAttribute("login", "failed");
+				res.getWriter().write("login failed");
+			}
+			
+				byte[] b="login failed".getBytes();
+				res.getOutputStream().write(b); // binary format response
+				
+				req.getSession().setAttribute("login", "failed");
+				req.getRequestDispatcher("error.jsp").forward(req, res);
+				
+			
+
+	} catch(Exception e) {
+		//e.printStackTrace();
+	}
+	}
 	
+			
+	
+	
+	
+	public void doPost() {
+		
+	}
+	
+	public void init() {
+		System.out.println("System is running for the first time.");
+	}
+	
+	public void destroy() {
+		System.out.println("This helps clearing all data.");
+	}
 }
